ee203375489edcf265aae573fc766025b240f2c2
Marques <davidmiguel.marques@cgi.com>
2023-03-15 01:52:47 +0000

    Update route form and deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import Dashboard from './pages/dashboard';
-import Login from './pages/login';
-import Reservations from './pages/reservations';
-import Settings from './pages/services';
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
+import Reservations from "./pages/reservations";
+import Settings from "./pages/services";
 
-import { QueryClient, QueryClientProvider } from 'react-query';
-import Services from './pages/services';
+import { QueryClient, QueryClientProvider } from "react-query";
+import Services from "./pages/services";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Dashboard />} />
           <Route path="/reservations" element={<Reservations />} />
           <Route path="/services" element={<Services />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   );
 }
